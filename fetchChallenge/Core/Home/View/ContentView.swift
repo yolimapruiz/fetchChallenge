69aa4f2b1f032b49eedec395f3ba0ff015aed0fd
yolimapruiz <yolimapereiraruiz@gmail.com>
2024-12-23 13:35:55 -0500

    Cleaner code without comments

--- fetchChallenge/Core/Home/View/ContentView.swift
+++ fetchChallenge/Core/Home/View/ContentView.swift
@@ -10,62 +10,6 @@ import SwiftUI
 struct ContentView: View {
     
     @ObservedObject private var viewModel: RecipesViewModel
-    
-    
-//    var recipes: [Recipe] {
-//        //recipesMock
-//        //viewModel.recipeList
-//        
-//    }
-    
-//    init(viewModel: RecipesViewModel) {
-//        self.viewModel = viewModel
-//    }
-//    
-//    var body: some View {
-//        ScrollView {
-//        VStack {
-//            Text("Recipes")
-//                .font(.title)
-//                .bold()
-//            
-//            if recipes.isEmpty {
-//                Spacer()
-//                Text("No Recipes are available")
-//                    .padding(.leading, 4)
-//                Spacer()
-//            } else {
-//                LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 15)], spacing: 15) {
-//                    
-//                    ForEach(recipes, id: \.id) {recipe in
-//                        CardView(recipe: recipe)
-//                    }
-//                }
-//            }
-//            
-//            
-//        }
-//    }
-//        .onAppear{
-//            
-////            //let urls = URLSessionHTTPClient(requestMaker: URLSessionRequestMaker())
-//            Task {
-//            //    let lista = await dataSource.getRecipesList()
-//                await viewModel.getRecipeList()
-//
-//            }
-//            
-//            
-//        }
-//        .refreshable {
-//            Task {
-//                await viewModel.getRecipeList()
-//            }
-//        }
-//    }
-//}
-
-//yol
 
     init(viewModel: RecipesViewModel) {
         self.viewModel = viewModel
